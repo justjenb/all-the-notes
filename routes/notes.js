238ebc91269c321d62df4af204f1dbd9188958ed
Jen B <jenbutler984@gmail.com>
2023-05-27 09:14:59 -0400

    Notes are being added but cannot view when clicked

---
+++ routes/notes.js
@@ -0,0 +1,30 @@
+const notes = require("express").Router();
+const { readFromFile, readAndAppend } = require("../helpers/fsUtils");
+const uuid = require("../helpers/uuid");
+
+// GET Route for retrieving all the notes
+notes.get("/", (req, res) => {
+  readFromFile("./db/db.json").then((data) => res.json(JSON.parse(data)));
+});
+
+// POST Route for a new UX/UI notes
+notes.post("/", (req, res) => {
+  console.log(req.body);
+
+  const { title, text } = req.body;
+
+  if (req.body) {
+    const newNote = {
+      title,
+      text,
+      note_id: uuid(),
+    };
+
+    readAndAppend(newNote, "./db/db.json");
+    res.json(`Note added successfully 🚀`);
+  } else {
+    res.error("Error in adding note");
+  }
+});
+
+module.exports = notes;
